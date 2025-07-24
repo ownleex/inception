2848e0d01747bccfdaffd4b726d600c9190ef3ed
ownleex <ownleex@gmail.com>
2025-07-25 00:31:17 +0200

    Refactor ftp_setup.sh: Simplify FTP user creation and remove unnecessary group modifications

--- srcs/requirements/bonus/ftp/tools/ftp_setup.sh
+++ srcs/requirements/bonus/ftp/tools/ftp_setup.sh
@@ -2,20 +2,15 @@
 
 # Créer l'utilisateur FTP
 echo "Création de l'utilisateur FTP: $FTP_USER"
-useradd -m -d /var/www/wordpress -s /bin/bash "$FTP_USER"
+useradd -m -d /var/www/wordpress -G www-data "$FTP_USER"
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-
-# Ajouter aux groupes nécessaires
-usermod -a -G www-data "$FTP_USER"
 
 # Permissions spéciales pour wp-content
 chmod -R 775 /var/www/wordpress
 chown -R "$FTP_USER:$FTP_USER" /var/www/wordpress
 
-# Créer les répertoires vsftpd
+# Créer le répertoire vsftpd
 mkdir -p /var/run/vsftpd/empty
-mkdir -p /var/log
-touch /var/log/vsftpd.log
 
 cat > /etc/vsftpd.conf << EOF
 listen=YES
