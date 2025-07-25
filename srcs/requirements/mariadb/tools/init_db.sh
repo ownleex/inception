81e3c3d40e5df6b46fa8b34160d87c31ede3bc88
ownleex <ownleex@gmail.com>
2025-07-25 21:26:15 +0200

    Refactor init_db.sh: Remove unnecessary comments to enhance clarity

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Préparer un script SQL à injecter lors du démarrage
 # @'%' permet à l'utilisateur de se connecter depuis les autres containers
-# flush privileges pour appliquer les changements
 cat << EOF > /tmp/init.sql
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';
@@ -10,6 +8,5 @@ GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-# Démarrer MariaDB avec le script d'init
 echo "Lancement de MariaDB avec injection SQL..."
 exec mysqld --init-file=/tmp/init.sql --datadir=/var/lib/mysql
